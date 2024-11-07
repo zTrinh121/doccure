354358d1ae42370c1526618f0e3857adfbda3bb6
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2024-11-07 11:13:52 +0700

    wip pagination appointment

--- FE/src/features/appointment/components/AppointmentList.jsx
+++ FE/src/features/appointment/components/AppointmentList.jsx
@@ -1,46 +1,83 @@
 import React from 'react';
 import AppointmentItem from './AppointmentItem';
-import { Pagination } from 'antd';
+import { Button, Pagination } from 'antd';
 import { useAppointmentsQuery } from '../../../hooks/useAppointmentsQuery';
 import IsPendingSpin from '../../../components/ui/IsPendingSpin';
 import { getTimeString } from '../../../utils/timeUtils';
+import { useState } from 'react';
+import { Spin } from 'antd';
 
 const AppointmentList = () => {
+  const [status, setStatus] = useState('');
+  const [pagination, setPagination] = useState({ page: 1, pageSize: 10 });
   const { isPending, isError, data, error } = useAppointmentsQuery({
-    // status: 'booked',
-    offset: 0,
-    limit: 4,
+    status: status,
+    offset: pagination.page,
+    limit: pagination.pageSize,
   });
 
-  if (isPending) {
-    return <IsPendingSpin></IsPendingSpin>;
-  }
+  // if (isPending) {
+  //   return <IsPendingSpin></IsPendingSpin>;
+  // }
+
+  const onChangePagination = (page, pageSize) => {
+    console.log('page', page, 'pagesize', pageSize);
+    setPagination({ page, pageSize });
+  };
+
+  const onClick = (value) => {
+    status === value ? setStatus('') : setStatus(value);
+  };
 
   return (
     <div>
-      <div>
-        {data.map((appointment) => {
-          return (
-            <>
-              {' '}
-              <AppointmentItem
-                avatar={appointment.doctor.avatar}
-                status={appointment.status}
-                price={appointment.price}
-                fullName={appointment.doctor.full_name}
-                time={getTimeString({
-                  date: appointment.slot.date_slot,
-                  start: appointment.slot.start_time,
-                  end: appointment.slot.end_time,
-                })}
-                appointmentId={appointment.appointment_id}
-                invoiceId={appointment.invoice.invoice_id}
-              />
-            </>
-          );
-        })}
-      </div>
-      <Pagination defaultCurrent={1} total={50} />
+      <Spin spinning={isPending}>
+        <div>
+          <Button
+            type={status === 'booked' ? 'primary' : ''}
+            onClick={() => {
+              onClick('booked');
+            }}
+          >
+            Booked
+          </Button>
+          <Button
+            type={status === 'pending_payment' ? 'primary' : ''}
+            onClick={() => {
+              onClick('pending_payment');
+            }}
+          >
+            Pending Payment
+          </Button>
+          {data?.map((appointment) => {
+            return (
+              <>
+                {' '}
+                <AppointmentItem
+                  avatar={appointment.doctor.avatar}
+                  status={appointment.status}
+                  price={appointment.price}
+                  fullName={appointment.doctor.full_name}
+                  time={getTimeString({
+                    date: appointment.slot.date_slot,
+                    start: appointment.slot.start_time,
+                    end: appointment.slot.end_time,
+                  })}
+                  appointmentId={appointment.appointment_id}
+                  invoiceId={appointment.invoice.invoice_id}
+                />
+              </>
+            );
+          })}
+        </div>
+      </Spin>
+      <Pagination
+        defaultCurrent={1}
+        total={50}
+        showSizeChanger
+        showTotal={(total) => `Total ${total} items`}
+        onChange={onChangePagination}
+      />
     </div>
   );
 };
--- FE/src/hooks/useAppointmentsQuery.js
+++ FE/src/hooks/useAppointmentsQuery.js
@@ -6,7 +6,11 @@ import { getAppointments } from "../lib/appointment";
 export const useAppointmentsQuery = ({ status, offset, limit }) => {
   const { isPending, isError, data, error } = useQuery({
     queryKey: ["appointments", status, offset, limit],
-    queryFn: async () => { return (await getAppointments({ status, offset, limit })).data.data },
+    queryFn: async () => {
+      const response = await getAppointments({ status, offset, limit });
+      console.log(response);
+      return response.data.data;
+    },
   });
   return { isPending, isError, data, error }
 }

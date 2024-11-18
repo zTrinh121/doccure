b0cb9d0e949fc4359e53920d051568d2f468ff0f
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2024-11-18 14:22:51 +0700

    Adding rating status for appoinment model
    Remove sensitive data and improve credential handling
    
    Adding rating status for appointment model and removing sensitive data

--- BE/src/main/java/com/doccure/BE/BeApplication.java
+++ BE/src/main/java/com/doccure/BE/BeApplication.java
@@ -3,8 +3,10 @@ package com.doccure.BE;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan({"com.doccure.BE.mapper"})
 public class BeApplication {
 
